0e900c905c1a7775a850afb29712c4415a7ceef6
hazel <yhj2756@naver.com>
2022-08-21 23:59:32 +0900

    undone task

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -26,12 +26,13 @@ const TodoHeaderBlock = styled.div`
 
 function TodoHeader() {
   const todos = useTodoState();
-  console.log(todos);
+  const undoneTasks = todos.filter(todo => !todo.done);
+
   return (
     <TodoHeaderBlock>
       <h1>2022년 8월 17일</h1>
       <div className="day">수요일</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </TodoHeaderBlock>
   );
 }
